1d1d8306e07248d4dc363302796656b9b7486166
mdmohid <alammohid855@gmail.com>
2026-09-05 16:44:02 +0545

    static folders created and implemented header and footer

--- decorshop/settings.py
+++ decorshop/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'myshop.context_processors.categories',
+                'myshop.context_processors.categories', #to make categories globally
             ],
         },
     },
